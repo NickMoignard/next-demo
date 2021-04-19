a749dac5fb34abd26eecfe7e80087ac393869035
NickMoignard <nicklmoignard@gmail.com>
2021-04-19 13:22:28 +1000

    prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,46 @@
 module.exports = {
-    // Settings detect react version
-    // ====================================================================
-    settings: {
-        react: {
-            version: "detect"
-        }
+  // Settings detect react version
+  // ====================================================================
+  settings: {
+    react: {
+      version: "detect",
     },
+  },
 
-    // Environment
-    // ====================================================================
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
+  // Environment
+  // ====================================================================
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  // Extends
+  // ====================================================================
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+
+  // Parser
+  // ====================================================================
+  // Typescript parser
+  // parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
 
-    // Extends
-    // ====================================================================
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
+  // Plugins
+  // ====================================================================
+  plugins: ["react"],
 
-    // Parser
-    // ====================================================================
-    // Typescript parser
-    // parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: 'module'
-    },
-
-    // Plugins
-    // ====================================================================
-    plugins: [
-        'react'
-    ],
-
-    // Rules Config
-    rules: {
-        'no-console': 'error',
-    }  
+  // Rules Config
+  rules: {
+    "no-console": "error",
+  },
 };
