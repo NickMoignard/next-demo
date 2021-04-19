bd978cfbfd440d6255e7ec5fe859b998e2c004c8
NickMoignard <nicklmoignard@gmail.com>
2021-04-19 13:14:13 +1000

    eslint setup for react

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    // Settings detect react version
+    // ====================================================================
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+
+    // Environment
+    // ====================================================================
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+
+    // Extends
+    // ====================================================================
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+
+    // Parser
+    // ====================================================================
+    // Typescript parser
+    // parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+
+    // Plugins
+    // ====================================================================
+    plugins: [
+        'react'
+    ],
+
+    // Rules Config
+    rules: {
+        'no-console': 'error',
+    }  
+};
